f6f5811aede96401abb47ac1295e1a0a64e784d2
Timid Robot Zehta <timidrobot@zehta.me>
2020-04-10 23:22:29 -0700

    updated based on `shellcheck` feedback

--- motleyfox.sh
+++ motleyfox.sh
@@ -36,22 +36,21 @@ PROFILES="${HOME}/Library/Application Support/Firefox/Profiles"
 
 
 help_print() {
-    # Print help/usage, then exit (incorrect usage should exit 2)
-    local _es=${1:-0}
+    # Print help/usage
     echo "${USAGE}"
-    exit ${_es}
+    exit
 }
 
 
 clone_firefox_app() {
-    local _name_title="${1}"
-    local _app="${_name_title}.app"
+    local _app _name_title
+    _name_title="${1}"
+    _app="${_name_title}.app"
     echo '# Copying Firefox application'
     echo "#   new application name: ${_name_title}"
     pushd /Applications >/dev/null
     # Remove previously created App Bundle
-    if [[ -d "${_app}" ]]
-    then
+    if [[ -d "${_app}" ]]; then
         rm -rf "${_app}"
     fi
     # Copy App Bundle
@@ -64,17 +63,20 @@ clone_firefox_app() {
 
 
 update_app_info() {
-    local _name_title="${1}"
-    local _name_lower="${2}"
-    local _icon_color="${3}"
-    local _app="${_name_title}.app"
-    local _path_app="/Applications/${_app}"
-    local _path_contents="${_path_app}/Contents"
-    local _icon_png="icons/icons8-firefox-480-${_icon_color}.png"
-    local _icon_icns="build/firefox-${_icon_color}.icns"
-    local _icon_icns_exists=0
-    local _icon_name="build/${name_lower}.icns"
-    local _icon_name_exists=0
+    local _app _icon_color _icon_icns _icon_name _icon_png _name_lower
+    local _name_title _path_app _path_contents
+    local -i _icon_icns_exists _icon_name_exists
+    _name_title="${1}"
+    _name_lower="${2}"
+    _icon_color="${3}"
+    _app="${_name_title}.app"
+    _path_app="/Applications/${_app}"
+    _path_contents="${_path_app}/Contents"
+    _icon_png="icons/icons8-firefox-480-${_icon_color}.png"
+    _icon_icns="build/firefox-${_icon_color}.icns"
+    _icon_icns_exists=0
+    _icon_name="build/${name_lower}.icns"
+    _icon_name_exists=0
     echo '# Updating application bundle'
     ## Update Info.plist
     echo '#   updating Info.plist'
@@ -86,35 +88,30 @@ update_app_info() {
         "${_path_contents}/Info.plist"
     rm -f "${_path_contents}/Info.plist.bak"
     # Generate Icon
-    if [[ -f "${_icon_name}" ]]
-    then
+    if [[ -f "${_icon_name}" ]]; then
         echo "#   using existing: ${_icon_name}"
-        local _icon_name_exists=1
-    elif [[ -f "${_icon_png}" ]]
-    then
+        _icon_name_exists=1
+    elif [[ -f "${_icon_png}" ]]; then
         echo "#   icon color: ${_icon_color}"
-        if [[ -f "${_icon_icns}" ]]
-        then
-            local _icon_icns_exists=1
+        if [[ -f "${_icon_icns}" ]]; then
+            _icon_icns_exists=1
         else
-            if which -s makeicns
-            then
+            if which -s makeicns; then
                 echo "#   generating: ${_icon_icns}"
                 echo "#     from: ${_icon_png}"
-                makeicns -in "${_icon_png}" -out ${_icon_icns}
-                local _icon_icns_exists=1
+                makeicns -in "${_icon_png}" -out "${_icon_icns}"
+                _icon_icns_exists=1
             else
                 echo '#   makeicns is not installed. Unable to generate icons.'
-                echo '#     (resovle with `brew install icns`)'
+                echo '#     (resolve with `brew install icns`)'
             fi
         fi
-        if (( _icon_icns_exists == 1 ))
-        then
+        if (( _icon_icns_exists == 1 )); then
             echo "#   creating symlinking: ${_icon_name}"
             echo "#     source: ${_icon_icns}"
             pushd build >/dev/null
             ln -s "${_icon_icns##*/}" "${_icon_name##*/}"
-            local _icon_name_exists=1
+            _icon_name_exists=1
             popd >/dev/null
         fi
     else
@@ -122,8 +119,7 @@ update_app_info() {
         echo '#     unable to generate icns file and symlink to it'
     fi
     # Replace Icon
-    if (( _icon_name_exists == 1 ))
-    then
+    if (( _icon_name_exists == 1 )); then
         echo '#   copying application icon into place'
         cp "build/${name_lower}.icns" \
             "${_path_contents}/Resources/firefox.icns"
@@ -139,14 +135,16 @@ update_app_info() {
 
 
 add_launch_script() {
-    local _name_title="${1}"
-    local _name_lower="${2}"
-    local _profiles="${3}"
-    local _app="${_name_title}.app"
-    local _path_app="/Applications/${_app}"
-    local _path_contents="${_path_app}/Contents"
-    local _path_macos="/Applications/${_app}/Contents/MacOS"
-    local _path_profile="${_profiles}/${_name_lower}"
+    local _app _name_lower _name_title _path_app _path_contents _path_macos
+    local _path_profile _profiles
+    _name_title="${1}"
+    _name_lower="${2}"
+    _profiles="${3}"
+    _app="${_name_title}.app"
+    _path_app="/Applications/${_app}"
+    _path_contents="${_path_app}/Contents"
+    _path_macos="/Applications/${_app}/Contents/MacOS"
+    _path_profile="${_profiles}/${_name_lower}"
     echo '# Creating launcher script'
     echo '#   located in app bundle Contents'
     {
@@ -156,32 +154,33 @@ add_launch_script() {
         echo "    --profile \\"
         echo "    '${_path_profile}'"
         #echo "    2>/dev/null &"
-    } > ${_path_contents}/${_name_lower}
-    chmod 0755 ${_path_contents}/${_name_lower}
+    } > "${_path_contents}/${_name_lower}"
+    chmod 0755 "${_path_contents}/${_name_lower}"
     echo "#   script name: ${_name_lower}"
 }
 
 
 create_profile() {
-    local _name_title="${1}"
-    local _name_lower="${2}"
-    local _label_title="${3}"
-    local _profiles="${4}"
-    local _app="${_name_title}.app"
-    local _path_macos="/Applications/${_app}/Contents/MacOS"
-    local _path_profile="${_profiles}/${_name_lower}"
+    local _app _label_title _name_lower _name_title _path_macos _path_profile
+    local _profiles _status
+    _name_title="${1}"
+    _name_lower="${2}"
+    _label_title="${3}"
+    _profiles="${4}"
+    _app="${_name_title}.app"
+    _path_macos="/Applications/${_app}/Contents/MacOS"
+    _path_profile="${_profiles}/${_name_lower}"
     echo '# Creating Firefox profile'
     echo "#   profile_path: ${_path_profile/${HOME}/~}"
     # Return immediately if profile already exists
-    if [[ -d "${_path_profile}" ]]
-    then
+    if [[ -d "${_path_profile}" ]]; then
         echo '#     no-op: profile already exists'
         return 0
     else
         printf '#   '
-        local _status=$("${_path_macos}/firefox-bin" -CreateProfile \
-                            "${_label_title} ${_path_profile}")
-        printf "${_status}"
+        _status=$("${_path_macos}/firefox-bin" -CreateProfile \
+                    "${_label_title} ${_path_profile}")
+        printf '%s' "${_status}"
     fi
 }
 
@@ -190,8 +189,7 @@ create_profile() {
 
 
 #### Parse options
-while [[ -n "${1:-}" ]]
-do
+while [[ -n "${1:-}" ]]; do
     case "${1}" in
         -h | -help | --help | Help | help )
             shift
@@ -212,8 +210,7 @@ do
 done
 (( ${#ARGS[@]} > 0 )) || ARGS=(Home:navy Work:gray)
 mkdir -p build
-for name_color in "${ARGS[@]}"
-do
+for name_color in "${ARGS[@]}"; do
     label_title=${name_color%%:*}
     icon_color=${name_color##*:}
     [[ "${label_title}" != "${icon_color}" ]] || icon_color='orange'
